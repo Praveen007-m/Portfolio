47e3824b0d9386d0b05d8cf0883e577b9b516913
Praveen007-m <praveensk3737@gmail.com>
2025-06-29 15:09:12 +0530

    feat: Added Project Section

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById("Resume").addEventListener("click", function () {
+  const link = document.createElement("a");
+  link.href = "file/PRAVEEN- MAIN RESUME.pdf";
+  link.download = "PRAVEEN - MAIN RESUME.pdf";   
+  link.click();
+});
+
+
+document.getElementById("Resume1").addEventListener("click", function () {
+  const link = document.createElement("a");
+  link.href = "file/PRAVEEN- MAIN RESUME.pdf";
+  link.download = "PRAVEEN - MAIN RESUME.pdf";   
+  link.click();
+});
